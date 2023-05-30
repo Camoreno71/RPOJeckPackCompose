2cf5705f841025a8930bbf8ad5d51749bc5500f9
Carlos Moreno <camc0308@gmail.com>
2023-05-29 21:14:04 -0500

    added buttons to navigate for the others activities

--- app/src/main/java/com/example/plan_lector/home.kt
+++ app/src/main/java/com/example/plan_lector/home.kt
@@ -1,20 +1,29 @@
 package com.example.plan_lector
 
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.border
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.Card
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
@@ -28,25 +37,50 @@ import com.example.plan_lector.ui.theme.Plan_lectorTheme
 
 @Composable
 fun Index(name: String, navigationController: NavHostController) {
+    val buttonLabels = listOf("Juegos", "Música", "Películas", "Tecnologías")
 
-    Column(
-        horizontalAlignment = Alignment.CenterHorizontally,
+    val buttonThemes = mapOf(
+        "Juegos" to Color.Red,
+        "Música" to Color.Yellow,
+        "Películas" to Color.Green,
+        "Tecnologías" to Color.Blue
+    )
+
+    val backgroundColor = Color(android.graphics.Color.parseColor("#22e6fe"))
+
+    Box(
         modifier = Modifier
             .fillMaxWidth()
             .fillMaxHeight()
-            .padding(20.dp)) {
-        Card(
+            .padding(20.dp),
+        contentAlignment = Alignment.Center
+    ) {
+        Image(
+            painter = painterResource(id = R.drawable.rop),
+            contentDescription = null,
+            contentScale = ContentScale.Crop,
+            modifier = Modifier
+                .fillMaxSize()
+                .background(Color.Transparent) // Establece el fondo transparente para la imagen
+        )
+        Column(
+            modifier = Modifier.padding(16.dp),
+            verticalArrangement = Arrangement.Center,
+            horizontalAlignment = Alignment.CenterHorizontally
         ) {
-            Image(painter = painterResource(id = R.drawable.background), contentDescription = null)
-            Column(modifier = Modifier.padding(10.dp)) {
-                Text("AB CDE", fontWeight = FontWeight.W700)
-                Text("+0 12345678")
-                Text("XYZ city", fontWeight = FontWeight.W300)
+            buttonLabels.forEach { label ->
+                Button(onClick = { /*TODO*/ },
+                    colors = ButtonDefaults.buttonColors(
+                        containerColor = backgroundColor
+                    )) {
+                    Text(label,
+                        color = Color.Black,
+                        modifier = Modifier.padding(vertical = 8.dp))
+                }
+                Spacer(modifier = Modifier.height(16.dp))
             }
         }
     }
-
-
 }
 
 @Preview(showBackground = true, showSystemUi = true)
